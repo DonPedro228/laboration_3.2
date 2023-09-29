96f5c0b8fd0c2eed73db2bcacbfe28378ea0b3e6
DonPedro228 <Roman.Kos.RI.2023@lpnu.ua>
2023-09-29 16:30:34 +0300

    branch_02 commit

--- if, else/solution.cpp
+++ if, else/solution.cpp
@@ -1,3 +1,5 @@
+/*
+//full 
 #include <iostream>
 using namespace std;
 
@@ -10,7 +12,7 @@ int main() {
 	if (x - 1 < 0 && b - x != 0) {
 		F = a * (x * x) + b;
 	}
-	else if (x - 1 > 0 and b + x == 0) {
+	else if (x - 1 > 0 && b + x == 0) {
 		F = x - a / x;
 	}
 	else {
@@ -19,3 +21,28 @@ int main() {
 	cout << "F = " << F << endl;
 	return 0;
 }
+*/
+/*
+//short
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, x, c, F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+	if (x - 1 < 0 && b - x != 0) {
+		F = a * (x * x) + b;
+	}
+	if (x - 1 > 0 && b + x == 0) {
+		F = x - a / x;
+	}
+	if (!(x - 1 < 0 && b - x != 0) && !(x - 1 > 0 && b + x == 0)) {
+		F = x / c;
+	}
+	cout << "F = " << F << endl;
+	return 0;
+}
+*/
